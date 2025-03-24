eae8ce4a3607c8c74e85a70bc90fd46ccc549aa0
reamted <nroussos2028@kehillahstudent.org>
2025-03-24 08:32:55 -0700

    here

--- q3.py
+++ q3.py
@@ -1,11 +1,38 @@
 # Read "romeo_and_juliet.txt" (The full text of Shakespeare's Romeo and Juliet)
-
+import re
 ####
 #### YOUR CODE HERE 
 ####
+def original_assighment():
+    total = 0
 
+    with open("romeo_and_juliet.txt", "r") as f:
+        for line in f:
+            total += line.count("juliet")
+
+    print(total)
 # Count how many times the word "Juliet" appears
 
 ####
 #### YOUR CODE HERE 
 ####
+def extension():
+    words = {}
+
+    with open("romeo_and_juliet.txt", "r") as f:
+        for line in f:
+            for word in line.split(" "):
+                if word in words:
+                    words[word] += 1
+                else:
+                    words[word] = 1
+
+    #for word in words:
+        #if words[word] > 100:
+            #print(word, words[word])
+
+    myKeys = list(words.keys())
+    myKeys.sort()
+
+    print(words)
+
